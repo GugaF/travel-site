a2f13d53cd2d202682a150e7659ba22b63b17d07
Guga <mightyguga@hotmail.com>
2017-08-11 16:47:40 +0800

    Completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,3 @@
-function Person(fullName, favColour) {
-	this.name = fullName;
-	this.favouriteColour = favColour;
-	this.greet = function() {
-		console.log("Hello, my name is " + this.name + 
-			" and my favourite colour is " + this.favouriteColour + ".");
-	}
-}
+import MobileMenu from './modules/MobileMenu';
 
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Person("Jane Smith", "green");
-jane.greet();
+var mobileMenu = new MobileMenu();
